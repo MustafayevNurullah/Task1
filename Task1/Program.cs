a5fe89925918c8ab10ede1f20546b8c8c0ecfe97
nurlan3295 <nurlan3295@gmail.com>
2020-12-02 14:07:27 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+
+       
+       
+        static void Main(string[] args)
+        {
+            while(true)
+            { 
+            Console.WriteLine("Enter number");
+            var number = Console.ReadLine();
+               
+                if (uint.TryParse(number, out uint  output) & number!=null)
+                {
+                    Console.WriteLine("Result");
+                      Single_Number(output);
+                break;
+                }
+                else
+                {
+                    Console.Clear();
+                    continue;
+                }
+            }
+
+
+
+            
+           
+
+             void Single_Number(uint number)
+            {
+                Console.Clear();
+                for (int i = 0; i < number; number--)
+                {
+                    if (number % 2 != 0)
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+            }
+        }
+    }
+}
